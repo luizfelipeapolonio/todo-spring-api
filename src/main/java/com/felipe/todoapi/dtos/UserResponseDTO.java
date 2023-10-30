0df965b2f235171a9ce16b7db86d606647ddd19f
Luiz Felipe <luizfelipe0527@gmail.com>
2023-10-29 23:21:38 -0300

    refactor: rename DTO

--- src/main/java/com/felipe/todoapi/dtos/UserResponseDTO.java
+++ src/main/java/com/felipe/todoapi/dtos/UserResponseDTO.java
@@ -2,7 +2,7 @@ package com.felipe.todoapi.dtos;
 
 import java.time.LocalDateTime;
 
-public record UserRegisterResponseDTO(
+public record UserResponseDTO(
   String id,
   String name,
   String email,
