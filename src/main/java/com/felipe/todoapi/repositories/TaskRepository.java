62f771be600ca8ed981737c054ae9ea19dcd5913
Luiz Felipe <luizfelipe0527@gmail.com>
2023-11-01 19:24:05 -0300

    feat: implement find all user tasks by user id function

--- src/main/java/com/felipe/todoapi/repositories/TaskRepository.java
+++ src/main/java/com/felipe/todoapi/repositories/TaskRepository.java
@@ -2,5 +2,13 @@ package com.felipe.todoapi.repositories;
 
 import com.felipe.todoapi.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface TaskRepository extends JpaRepository<Task, String> {}
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, String> {
+
+  @Query("SELECT t FROM Task t WHERE t.user.id = :id")
+  List<Task> findAllByUserId(@Param("id") String id);
+}
