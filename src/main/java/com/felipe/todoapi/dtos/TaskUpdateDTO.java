8ef28e1d1d63bd326c107b36f32f681179fc12af
Luiz Felipe <luizfelipe0527@gmail.com>
2023-11-06 22:13:13 -0300

    improvement: add custom validation message and priority validator annotation

--- src/main/java/com/felipe/todoapi/dtos/TaskUpdateDTO.java
+++ src/main/java/com/felipe/todoapi/dtos/TaskUpdateDTO.java
@@ -1,13 +1,13 @@
 package com.felipe.todoapi.dtos;
 
+import com.felipe.todoapi.enums.PriorityLevel;
+import com.felipe.todoapi.enums.validation.ValueOfPriorityLevel;
 import jakarta.annotation.Nullable;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Positive;
 import org.hibernate.validator.constraints.Length;
 
 public record TaskUpdateDTO(
-  @Nullable @Length(min = 1, max = 60) String title,
+  @Nullable @Length(min = 1, max = 60, message = "O título deve ter entre 1 e 60 caracteres") String title,
   @Nullable String description,
-  @Nullable @Positive @Max(3) Integer priority,
+  @Nullable @ValueOfPriorityLevel(enumClass = PriorityLevel.class) String priority,
   @Nullable Boolean isDone
 ) {}
