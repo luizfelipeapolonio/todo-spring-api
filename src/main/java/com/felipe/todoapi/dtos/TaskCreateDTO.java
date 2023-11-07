fb68bb0181f2e4c53f5cb4bc3d6c12e029041b1a
Luiz Felipe <luizfelipe0527@gmail.com>
2023-11-06 22:07:51 -0300

    improvement: add custom validation messages and priority validator annotation

--- src/main/java/com/felipe/todoapi/dtos/TaskCreateDTO.java
+++ src/main/java/com/felipe/todoapi/dtos/TaskCreateDTO.java
@@ -1,14 +1,22 @@
 package com.felipe.todoapi.dtos;
 
+import com.felipe.todoapi.enums.PriorityLevel;
+import com.felipe.todoapi.enums.validation.ValueOfPriorityLevel;
 import jakarta.annotation.Nullable;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import org.hibernate.validator.constraints.Length;
 
 public record TaskCreateDTO(
-  @NotNull @NotBlank @Length(min = 1, max = 60) String title,
+  @NotNull(message = "O título é obrigatório")
+  @NotBlank(message = "O título não pode estar em branco")
+  @Length(min = 1, max = 60, message = "O título deve ter entre 1 e 60 caracteres")
+  String title,
+
   @Nullable String description,
-  @NotNull @Positive @Max(3) Integer priority
+
+  @NotNull(message = "A prioridade é obrigatória")
+  @NotBlank(message = "A prioridade não deve estar em branco")
+  @ValueOfPriorityLevel(enumClass = PriorityLevel.class)
+  String priority
 ) {}
