110fded7b517a50c579e32289993436a7bbfff39
Luiz Felipe <luizfelipe0527@gmail.com>
2023-11-06 21:57:32 -0300

    feat: add priority level enum converter

---
+++ src/main/java/com/felipe/todoapi/enums/converters/PriorityLevelConverter.java
@@ -0,0 +1,30 @@
+package com.felipe.todoapi.enums.converters;
+
+import com.felipe.todoapi.enums.PriorityLevel;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.util.stream.Stream;
+
+@Converter(autoApply = true)
+public class PriorityLevelConverter implements AttributeConverter<PriorityLevel, String> {
+
+  @Override
+  public String convertToDatabaseColumn(PriorityLevel priorityLevel) {
+    if(priorityLevel == null) {
+      return null;
+    }
+    return priorityLevel.getValue();
+  }
+
+  @Override
+  public PriorityLevel convertToEntityAttribute(String value) {
+    if(value == null) {
+      return null;
+    }
+    return Stream.of(PriorityLevel.values())
+      .filter(priorityLevel -> priorityLevel.getValue().equals(value))
+      .findFirst()
+      .orElseThrow(() -> new IllegalArgumentException("Valor de Enum inválido: " + value));
+  }
+}
