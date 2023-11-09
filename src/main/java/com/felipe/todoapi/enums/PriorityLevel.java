69960b7a0b5c96cd1a25fedc5b437062309d5cf8
Luiz Felipe <luizfelipe0527@gmail.com>
2023-11-08 23:38:08 -0300

    improvement: add level property to each enum

--- src/main/java/com/felipe/todoapi/enums/PriorityLevel.java
+++ src/main/java/com/felipe/todoapi/enums/PriorityLevel.java
@@ -1,16 +1,22 @@
 package com.felipe.todoapi.enums;
 
 public enum PriorityLevel {
-  LOW("baixa"), MEDIUM("media"), HIGH("alta");
+  LOW("baixa", "3"), MEDIUM("media", "2"), HIGH("alta", "1");
 
   private final String value;
+  private final String level;
 
-  PriorityLevel(String value) {
+  PriorityLevel(String value, String level) {
     this.value = value;
+    this.level = level;
   }
 
   public String getValue() {
     return this.value;
+  }
+
+  public String getLevel() {
+    return this.level;
   }
 
   @Override
