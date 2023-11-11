ac30c42ec5fe7684d8b23edf3093276180b283ce
Luiz Felipe <luizfelipe0527@gmail.com>
2023-11-11 19:17:36 -0300

    test(TaskReposiotry): implement findAllDoneOrNotDoneTask test

--- src/test/java/com/felipe/todoapi/repositories/TaskRepositoryTest.java
+++ src/test/java/com/felipe/todoapi/repositories/TaskRepositoryTest.java
@@ -6,13 +6,13 @@ import com.felipe.todoapi.enums.PriorityLevel;
 import com.felipe.todoapi.models.Task;
 import com.felipe.todoapi.models.User;
 import jakarta.persistence.EntityManager;
-import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import java.util.UUID;
@@ -29,20 +29,40 @@ public class TaskRepositoryTest {
 
   @Test
   @DisplayName("Should return all tasks belonging to the given user id")
-  void findAllByUserId() {
+  void findAllTasksByUserId() {
     User user1 = this.createUser(new UserRegisterDTO("User 1","teste1@email.com","123456"));
     User user2 = this.createUser(new UserRegisterDTO("User 2","teste2@email.com","123456"));
 
-    this.createTask(new TaskCreateDTO("Task 1","Descrição 1","baixa"), user1);
-    this.createTask(new TaskCreateDTO("Task 2","Descrição 2","alta"), user1);
-    this.createTask(new TaskCreateDTO("Task 3","Descrição 3","media"), user2);
+    this.createTask(new TaskCreateDTO("Task 1","Descrição 1","baixa"), false, user1);
+    this.createTask(new TaskCreateDTO("Task 2","Descrição 2","alta"), false, user1);
+    this.createTask(new TaskCreateDTO("Task 3","Descrição 3","media"), false, user2);
 
     List<Task> tasks = this.taskRepository.findAllByUserId(user1.getId(), Sort.by("title").ascending());
 
-    assertThat(tasks).allSatisfy(task -> assertThat(task.getUser().getId()).isEqualTo(user1.getId()));
+    assertThat(tasks)
+      .allSatisfy(task -> assertThat(task.getUser().getId()).isEqualTo(user1.getId()))
+      .hasSize(2);
   }
 
-  private void createTask(TaskCreateDTO data, User user) {
+  @Test
+  @DisplayName("Should return all tasks done or not done belonging to the given user id, according to the given task status")
+  void findAllDoneOrNotDoneTasksByUserId() {
+    User user1 = this.createUser(new UserRegisterDTO("User 1","teste1@email.com","123456"));
+    User user2 = this.createUser(new UserRegisterDTO("User 2","teste2@email.com","123456"));
+
+    this.createTask(new TaskCreateDTO("Task 1","Descrição 1","baixa"), true, user1);
+    this.createTask(new TaskCreateDTO("Task 2","Descrição 2","alta"), true, user1);
+    this.createTask(new TaskCreateDTO("Task 3","Descrição 3","media"), false, user2);
+
+    List<Task> tasks = this.taskRepository.findAllDoneOrNotDone(user1.getId(), true);
+
+    assertThat(tasks).allSatisfy(task -> {
+      assertThat(task.getUser().getId()).isEqualTo(user1.getId());
+      assertThat(task.isDone()).isEqualTo(true);
+    }).hasSize(2);
+  }
+
+  private void createTask(TaskCreateDTO data, boolean isTaskDone, User user) {
     UUID randomUUID = UUID.randomUUID();
     String id = randomUUID.toString();
 
@@ -51,6 +71,7 @@ public class TaskRepositoryTest {
     newTask.setTitle(data.title());
     newTask.setDescription(data.description());
     newTask.setPriority(this.convertPriorityLevelValue(data.priority()));
+    newTask.setIsDone(isTaskDone);
     newTask.setUser(user);
 
     this.entityManager.merge(newTask);
