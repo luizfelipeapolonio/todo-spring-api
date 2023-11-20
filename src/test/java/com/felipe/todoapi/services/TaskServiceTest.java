aa7533de8707f0dc6ced9d7b93cf9980f7e870b3
Luiz Felipe <luizfelipe0527@gmail.com>
2023-11-19 22:18:16 -0300

    test(TaskService): implement findTaskByIdFailByTaskNotFound test

--- src/test/java/com/felipe/todoapi/services/TaskServiceTest.java
+++ src/test/java/com/felipe/todoapi/services/TaskServiceTest.java
@@ -330,6 +330,25 @@ public class TaskServiceTest {
     verify(this.taskMapper, never()).toDTO(any(Task.class));
   }
 
+  @Test
+  @DisplayName("Should throw a RecordNotFoundException when the task does not exist")
+  void findTaskByIdFailByTaskNotFound() throws RecordNotFoundException {
+    UserSpringSecurity authUser = new UserSpringSecurity("01", "teste1@email.com", "123456");
+
+    this.mockAuthentication(authUser);
+    when(this.taskRepository.findById(anyString())).thenReturn(Optional.empty());
+
+    Exception thrown = catchException(() -> this.taskService.findById("01"));
+
+    assertThat(thrown)
+      .isExactlyInstanceOf(RecordNotFoundException.class)
+      .hasMessage("Tarefa não encontrada");
+
+    verify(this.securityContext, times(1)).getAuthentication();
+    verify(this.taskRepository, times(1)).findById(anyString());
+    verify(this.taskMapper, never()).toDTO(any(Task.class));
+  }
+
   private List<Task> generateTaskList(User user) {
     List<Task> tasks = new ArrayList<>();
 
